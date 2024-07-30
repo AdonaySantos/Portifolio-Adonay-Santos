13a064f4f0f26995539d29cd114deb3474c239b0
Adonay Santos <adonay.rssantos@gmail.com>
2024-07-29 23:40:55 -0300

    Arquivos que precisam de desenvolvimento

---
+++ src/pages/Contato.jsx
@@ -0,0 +1,9 @@
+export default function Contato() {
+    return (
+        <>
+            <h1>Fale comigo</h1>
+            <label htmlFor="Digite seu nome:" id="nome"></label>
+            <input type="text" id="nome"/>
+        </>
+    )
+}
