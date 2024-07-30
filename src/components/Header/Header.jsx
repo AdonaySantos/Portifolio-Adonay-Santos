0477e40d5f41a2db5134af284af2c3721fe516f5
Adonay Santos <adonay.rssantos@gmail.com>
2024-07-29 23:39:10 -0300

    Componente fixo  `Header` criado e estilizado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,26 @@
-import asset from '../../assets/react.svg'
 import { Link } from 'react-router-dom'
 import './Header.css'
+import favicon from '../../assets/favicon.ico'
+
 
 export default function Header(){
+    const buttonlink = () => {
+        window.location.href = 'https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:0d9dc94a-147d-4619-b028-5a64d26dfbb2'
+    };
+
+
     return (
         <header>
-            <img src={asset} alt="" />
-            <nav>
+            <img src={favicon} alt="logo" className='logo'/>
+            <nav className='links'>
                 <Link to='/'>Home</Link>
-                <Link to='/About'>About</Link>
+                <Link>About</Link>
+                <Link>Projects</Link>
+                <Link to='/Contato'>Contact</Link>
             </nav>
+            <div className='cv-button'>
+                <a href={buttonlink}>Currículo </a>
+            </div>
         </header>
     )
 }
