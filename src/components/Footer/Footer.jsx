93c1c5b46a0981d63a48b4811258cc65e748cf62
Adonay Santos <adonay.rssantos@gmail.com>
2024-07-30 22:27:57 -0300

    Criação e estilização do componente `Footer`

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,16 @@
-import './Footer.css'
+import { Link } from 'react-router-dom';
+import githubicon from '../../assets/developer-white.ico'
+import './Footer.css';
 
 export default function Footer() {
+    const adonaySantos = "https://github.com/AdonaySantos"
+
     return (
-        <>
-            
-        </>
-    )
+        <footer className='container-footer'>
+            <div className='contact-links'>
+                    <Link to={adonaySantos}><img src={githubicon} alt="GitHub" className='git-icon'/></Link>
+            </div>
+            <p className='text-footer'>© {new Date().getFullYear()} Adonay Santos. <u> Crafted by yours truly </u></p>
+        </footer>
+    );
 }
