99eee7dee2edc045f774ebabae8f2b7c74680fe3
Adonay Santos <adonay.rssantos@gmail.com>
2024-07-30 22:29:21 -0300

    Atualização na `Home.jsx`

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,14 +2,19 @@ import backroundImg from '../assets/background-home.jpg'
 import './home.css'
 
 export default function Home(){
+    
+
     return (
         <>
-            <div className='home'>
-                <div id='background'>
+            <section className='home'>
+                <div id='home-content'>
                     <img src={backroundImg} alt="" />
-                    <h1 className='texto'>Adonay Santos <br /> Full Stack Developer</h1>
+                    <div className='text-container'>
+                        <h2 className='texto-h2'>Adonay Santos</h2>
+                        <h1 className='texto-h1'>Full Stack Developer</h1>
+                    </div>
                 </div>
-            </div>
+            </section>
         </>
     )
 }
