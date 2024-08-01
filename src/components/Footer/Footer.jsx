8f77349290c2a5db7239b3b37dc880a22196dfbc
Adonay Santos <adonay.rssantos@gmail.com>
2024-08-01 19:20:36 -0300

    Atualização de estrutura e de estilo do Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import githubicon from '../../assets/developer-white.ico'
+import githubicon from '../../assets/developer.ico'
 import './Footer.css';
 
 export default function Footer() {
@@ -7,10 +7,10 @@ export default function Footer() {
 
     return (
         <footer className='container-footer'>
+            <p className='text-footer'>© {new Date().getFullYear()} Adonay Santos. <u> Crafted by yours truly </u></p>
             <div className='contact-links'>
                     <Link to={adonaySantos}><img src={githubicon} alt="GitHub" className='git-icon'/></Link>
             </div>
-            <p className='text-footer'>© {new Date().getFullYear()} Adonay Santos. <u> Crafted by yours truly </u></p>
         </footer>
     );
 }
