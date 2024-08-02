d415c981b364305dfdf34a41da587cb3cb00ded3
Adonay Santos <adonay.rssantos@gmail.com>
2024-08-01 21:24:37 -0300

    Componente `About` criado e estilizado

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,26 @@
-export default function About(){
+import './About.css'
+
+export default function About() {
     return (
         <>
-        
+            <section className='about'>
+                <div className='about-container1'>
+                    <h2 className="h2-about">Hi there</h2>
+                    <p className="p-about">
+                    I'm Adonay, currently I live in São Paulo working as a <br />
+                    Full-Stack freelancer. My projects include WebDevelopment <br />
+                    with CRUD in memory using MongoDB or MySQL. I'm looking for <br />
+                    a job, if you see my portfolio and you're interested, send <br />
+                    me a message.
+                    </p>
+                    <p className="p-about">
+                    In my spare time i like to practice English, play video <br />
+                    games, to read, and solve logical problems with code. <br />
+                    I'm always down for hearing about new projects, problems or <br />
+                    some stuff like that
+                    </p>
+                </div>
+            </section>
         </>
-    )
+  );
 }
