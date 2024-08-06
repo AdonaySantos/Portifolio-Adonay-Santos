0e4ccee01f2338ae77825e654aebb400ed99ca0e
Adonay Santos <adonay.rssantos@gmail.com>
2024-08-05 23:19:19 -0300

    Criação do forms da página de contato

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,8 +1,24 @@
+import './Contato.css'
+
 export default function Contato() {
+
     return (
         <>
             <section>
-                
+                <div id="contact-content">
+                    <div className='form-container'>
+                        <h2>Say hello</h2>
+                        <div className='form-content'>
+                            <label htmlFor="email" className='email'>Email</label>
+                            <input type="email" className='email'/><br />
+                            <div className='underline'></div>
+                            <label htmlFor="text" className='text'>Message</label>
+                            <input type="text" /><br />
+                            <div className='underline'></div>
+                        </div>
+                        <button className='send-message'>Send</button>
+                    </div>
+                </div>
             </section>
         </>
     )
